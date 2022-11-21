61c77533e631d3ad4b4644f7f3687fbd117ed553
mbugua <mbuguafaimba@gmail.com>
2022-11-21 20:54:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+var num1 = 31
+var num2 = 2
+var multiply = num1 * num2
+const random = Math.floor(Math.random() * 1000) + 1;
+
+var num3 = 16
+var num4 = 6
+var mod = num3 % num4
+
+const max = Math.max(4, 13, 20, 18, 9)
